a4dded835558c5e0defbc167d4295607c09c702a
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-15 21:22:59 -0500

    Mocking complete dependency bean for test to avoid search of non-existent implementation

--- social-multiplication/src/test/java/microservices/book/multiplication/service/MultiplicationServiceTest.java
+++ social-multiplication/src/test/java/microservices/book/multiplication/service/MultiplicationServiceTest.java
@@ -2,9 +2,13 @@ package microservices.book.multiplication.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 public class MultiplicationServiceTest {
+
+    @MockBean
+    private RandomGeneratorService randomGeneratorService;
 
     @Test
     public void createRandomMultiplicationTest() {
