9e9c9e95f50465a8241c79f6fa469c1de0fe3bc3
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-16 12:04:20 -0500

    Unit test for multiplication service implementation

---
+++ social-multiplication/src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
@@ -0,0 +1,37 @@
+package microservices.book.multiplication.service;
+
+import microservices.book.multiplication.domain.Multiplication;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mock;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class MultiplicationServiceImplTest {
+
+    @Mock
+    private RandomGeneratorService randomGeneratorService;
+    private MultiplicationServiceImpl multiplicationService;
+
+    @BeforeAll
+    public void setUp() {
+        multiplicationService = new MultiplicationServiceImpl(randomGeneratorService);
+    }
+
+    @Test
+    public void createRandomMultiplication() {
+
+        given(randomGeneratorService.generateRandomFactor())
+                .willReturn(50, 30);
+
+        Multiplication randomMultiplication = multiplicationService.createRandomMultiplication();
+
+        assertThat(randomMultiplication.getFactorA()).isEqualTo(50);
+        assertThat(randomMultiplication.getFactorB()).isEqualTo(30);
+        assertThat(randomMultiplication.getResult()).isEqualTo(1500);
+    }
+}
