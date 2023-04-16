2225f0b8e31f7b2a717a81d328e50699173f4c4f
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-15 21:20:46 -0500

    Test class for multiplication service

---
+++ social-multiplication/src/test/java/microservices/book/multiplication/service/MultiplicationServiceTest.java
@@ -0,0 +1,14 @@
+package microservices.book.multiplication.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MultiplicationServiceTest {
+
+    @Test
+    public void createRandomMultiplicationTest() {
+
+    }
+
+}
