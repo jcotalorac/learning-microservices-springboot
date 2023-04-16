eadb538de94854b7d594f6568714a40207d341f9
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-16 11:46:38 -0500

    Logic implementation to generate random factor using one index range and their corresponding calculations

--- social-multiplication/src/main/java/microservices/book/multiplication/service/RandomGeneratorServiceImpl.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/RandomGeneratorServiceImpl.java
@@ -2,10 +2,15 @@ package microservices.book.multiplication.service;
 
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+
 @Service
 public class RandomGeneratorServiceImpl implements RandomGeneratorService {
+    private static final int MINIMUM_FACTOR = 11;
+    private static final int MAXIMUM_FACTOR = 99;
+
     @Override
     public int generateRandomFactor() {
-        return 0;
+        return new SecureRandom().nextInt((MAXIMUM_FACTOR - MINIMUM_FACTOR) + 1) + MINIMUM_FACTOR;
     }
 }
