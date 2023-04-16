66eeb686815d3b58af42a9fc277569d682945797
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-16 16:51:13 -0500

    Converting domain multiplication to immutable object using lombok annotations

--- social-multiplication/src/main/java/microservices/book/multiplication/domain/Multiplication.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/domain/Multiplication.java
@@ -1,47 +1,16 @@
 package microservices.book.multiplication.domain;
 
-public class Multiplication {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
-    private int factorA;
-    private int factorB;
-    private int result;
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class Multiplication {
 
-    public Multiplication(int factorA, int factorB) {
-        this.factorA = factorA;
-        this.factorB = factorB;
-        this.result = factorA * factorB;
-    }
-
-    public int getFactorA() {
-        return factorA;
-    }
-
-    public void setFactorA(int factorA) {
-        this.factorA = factorA;
-    }
-
-    public int getFactorB() {
-        return factorB;
-    }
-
-    public void setFactorB(int factorB) {
-        this.factorB = factorB;
-    }
-
-    public int getResult() {
-        return result;
-    }
-
-    public void setResult(int result) {
-        this.result = result;
-    }
-
-    @Override
-    public String toString() {
-        return "Multiplication{" +
-                "factorA=" + factorA +
-                ", factorB=" + factorB +
-                ", result(A*B)=" + result +
-                '}';
-    }
+    private final int factorA;
+    private final int factorB;
 }
