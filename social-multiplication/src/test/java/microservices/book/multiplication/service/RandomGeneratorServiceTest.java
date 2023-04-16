4a583f0fb2b6142729fffe85208654b454be1f9c
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-16 09:28:54 -0500

    Basic test class for random generator service

---
+++ social-multiplication/src/test/java/microservices/book/multiplication/service/RandomGeneratorServiceTest.java
@@ -0,0 +1,12 @@
+package microservices.book.multiplication.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RandomGeneratorServiceTest {
+
+    @Test
+    public void generateRandomFactor() {
+    }
+}
