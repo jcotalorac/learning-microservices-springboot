f916077a0928dc517c7ea8a93423a3eeb48d072e
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-16 10:14:03 -0500

    Adjusting test case

--- social-multiplication/src/test/java/microservices/book/multiplication/service/RandomGeneratorServiceTest.java
+++ social-multiplication/src/test/java/microservices/book/multiplication/service/RandomGeneratorServiceTest.java
@@ -1,12 +1,18 @@
 package microservices.book.multiplication.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 public class RandomGeneratorServiceTest {
 
+    @Autowired
+    private RandomGeneratorService randomGeneratorService;
+
     @Test
-    public void generateRandomFactor() {
+    public void generateRandomFactorIsBetweenExpectedLimits() {
     }
 }
