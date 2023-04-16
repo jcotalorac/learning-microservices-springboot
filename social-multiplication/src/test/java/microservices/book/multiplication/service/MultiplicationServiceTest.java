b3f40813512c13518cc18bd8c534974b6fa6ee68
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-15 21:26:00 -0500

    Class under test injection

--- social-multiplication/src/test/java/microservices/book/multiplication/service/MultiplicationServiceTest.java
+++ social-multiplication/src/test/java/microservices/book/multiplication/service/MultiplicationServiceTest.java
@@ -1,6 +1,7 @@
 package microservices.book.multiplication.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -9,6 +10,8 @@ public class MultiplicationServiceTest {
 
     @MockBean
     private RandomGeneratorService randomGeneratorService;
+    @Autowired
+    private MultiplicationService multiplicationService;
 
     @Test
     public void createRandomMultiplicationTest() {
