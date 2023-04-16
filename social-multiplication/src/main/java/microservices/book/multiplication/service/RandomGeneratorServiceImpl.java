14df46682c2bf53d37eaad2bd402f4f6a31266c6
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-16 11:30:28 -0500

    Initial implementation of random generator service

---
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/RandomGeneratorServiceImpl.java
@@ -0,0 +1,11 @@
+package microservices.book.multiplication.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RandomGeneratorServiceImpl implements RandomGeneratorService {
+    @Override
+    public int generateRandomFactor() {
+        return 0;
+    }
+}
