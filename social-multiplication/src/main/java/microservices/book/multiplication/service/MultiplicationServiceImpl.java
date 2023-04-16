77c4d3aaaaaadb577f31950f9250db16bc6e11cc
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-15 21:55:39 -0500

    Prototype for implementation of multiplication service

---
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationServiceImpl.java
@@ -0,0 +1,12 @@
+package microservices.book.multiplication.service;
+
+import microservices.book.multiplication.domain.Multiplication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MultiplicationServiceImpl implements MultiplicationService {
+    @Override
+    public Multiplication createRandomMultiplication() {
+        return null;
+    }
+}
