9c80c5ca2c3ae8fa7cd30fe282ee513b0375cce5
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-16 10:35:22 -0500

    Then clause, to state that generated random factors must be between limits

--- social-multiplication/src/test/java/microservices/book/multiplication/service/RandomGeneratorServiceTest.java
+++ social-multiplication/src/test/java/microservices/book/multiplication/service/RandomGeneratorServiceTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class RandomGeneratorServiceTest {
@@ -22,5 +22,8 @@ public class RandomGeneratorServiceTest {
         List<Integer> randomFactors = IntStream.range(0, 1000)
                 .map(i -> randomGeneratorService.generateRandomFactor()).boxed()
                 .collect(Collectors.toList());
+
+        assertThat(randomFactors).containsOnlyElementsOf(IntStream.range(11, 100)
+                .boxed().collect(Collectors.toList()));
     }
 }
