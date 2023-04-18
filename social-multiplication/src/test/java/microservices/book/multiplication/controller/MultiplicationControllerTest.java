dea7d56377be9edc75389d87edb49f26bb6116e7
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-17 21:30:46 -0500

    Mocking multiplication service to test without implementations

--- social-multiplication/src/test/java/microservices/book/multiplication/controller/MultiplicationControllerTest.java
+++ social-multiplication/src/test/java/microservices/book/multiplication/controller/MultiplicationControllerTest.java
@@ -1,7 +1,11 @@
 package microservices.book.multiplication.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import microservices.book.multiplication.service.MultiplicationService;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 public class MultiplicationControllerTest {
+
+    @MockBean
+    private MultiplicationService multiplicationService;
 
 }
