0ab11bee9e12ade5c6e3750e34a70b18ab3691c2
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-17 22:34:41 -0500

    Then clause for assertions over controller output as status and json response

--- social-multiplication/src/test/java/microservices/book/multiplication/controller/MultiplicationControllerTest.java
+++ social-multiplication/src/test/java/microservices/book/multiplication/controller/MultiplicationControllerTest.java
@@ -10,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
@@ -42,5 +44,9 @@ public class MultiplicationControllerTest {
         MockHttpServletResponse response = mockMvc.perform(get("/multiplications/random")
                         .accept(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getContentAsString())
+                .isEqualTo(json.write(new Multiplication(70, 20)).getJson());
     }
 }
