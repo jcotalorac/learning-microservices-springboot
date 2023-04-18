b2c6aade190f59ce4e86561d766b00832a6f81a9
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-18 11:41:41 -0500

    Mvc controller test for multiplication result attempt controller

---
+++ social-multiplication/src/test/java/microservices/book/multiplication/controller/MultiplicationResultAttemptControllerTest.java
@@ -0,0 +1,24 @@
+package microservices.book.multiplication.controller;
+
+import microservices.book.multiplication.service.MultiplicationService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(MultiplicationResultAttemptController.class)
+public class MultiplicationResultAttemptControllerTest {
+
+    @MockBean
+    private MultiplicationService multiplicationService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void postResultReturnCorrect(){}
+
+    @Test
+    public void postResultReturnNotCorrect(){}
+}
