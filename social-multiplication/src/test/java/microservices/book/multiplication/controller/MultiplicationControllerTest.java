a372bcfb143c6f3c2420c24603bc05a4872769bb
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-17 21:50:19 -0500

    Setting test as a controller test. WebMvcTest only loads the configuration related with controller, instead of SpringBootTest which loads the entire configuration

--- social-multiplication/src/test/java/microservices/book/multiplication/controller/MultiplicationControllerTest.java
+++ social-multiplication/src/test/java/microservices/book/multiplication/controller/MultiplicationControllerTest.java
@@ -1,11 +1,18 @@
 package microservices.book.multiplication.controller;
 
 import microservices.book.multiplication.service.MultiplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+@WebMvcTest(MultiplicationController.class)
 public class MultiplicationControllerTest {
 
     @MockBean
     private MultiplicationService multiplicationService;
+
+    @Autowired
+    private MockMvc mockMvc;
 
 }
