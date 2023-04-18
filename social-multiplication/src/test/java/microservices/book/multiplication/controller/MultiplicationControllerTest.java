fbc916a080d5d776a3667926b081ceac3d0543b1
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-17 22:20:08 -0500

    Setting up the jackson tester object for this suite

--- social-multiplication/src/test/java/microservices/book/multiplication/controller/MultiplicationControllerTest.java
+++ social-multiplication/src/test/java/microservices/book/multiplication/controller/MultiplicationControllerTest.java
@@ -1,8 +1,11 @@
 package microservices.book.multiplication.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import microservices.book.multiplication.domain.Multiplication;
 import microservices.book.multiplication.service.MultiplicationService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
@@ -10,6 +13,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(MultiplicationController.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MultiplicationControllerTest {
 
     @MockBean
@@ -18,6 +22,11 @@ public class MultiplicationControllerTest {
     @Autowired
     private MockMvc mockMvc;
     private JacksonTester<Multiplication> json;
+
+    @BeforeAll
+    public void setUp() {
+        JacksonTester.initFields(this, new ObjectMapper());
+    }
 
     @Test
     public void getRandomMultiplicationTest() {}
