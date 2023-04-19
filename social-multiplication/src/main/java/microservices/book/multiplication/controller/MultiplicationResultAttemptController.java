f33740b8b2a088ddbca569ba6a48f7c6ea30d622
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-04-18 21:34:51 -0500

    Basic implementation of post mapping method to sent multiplication result attempt

--- social-multiplication/src/main/java/microservices/book/multiplication/controller/MultiplicationResultAttemptController.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/controller/MultiplicationResultAttemptController.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import microservices.book.multiplication.service.MultiplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,11 @@ public final class MultiplicationResultAttemptController {
     @Autowired
     public MultiplicationResultAttemptController(final MultiplicationService multiplicationService) {
         this.multiplicationService = multiplicationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResultResponse> postResult(){
+        return null;
     }
 
     @RequiredArgsConstructor
