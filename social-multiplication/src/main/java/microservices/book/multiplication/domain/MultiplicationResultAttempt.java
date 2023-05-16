ec93c877c8d5dfdcaf6cbaa238af45cf3e52fb9e
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-05-16 12:05:11 -0500

    Referencing column names of the multiplication and user entities linked with this one (multiplication result attempt)

--- social-multiplication/src/main/java/microservices/book/multiplication/domain/MultiplicationResultAttempt.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/domain/MultiplicationResultAttempt.java
@@ -18,8 +18,10 @@ public final class MultiplicationResultAttempt {
     private Long id;
 
     @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "USER_ID")
     private final User user;
     @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "MULTIPLICATION_ID")
     private final Multiplication multiplication;
     private final int resultAttempt;
     private final boolean correct;
