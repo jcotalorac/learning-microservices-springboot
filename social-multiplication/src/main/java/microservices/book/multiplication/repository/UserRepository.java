f305721982e4b36a2fd6c9d4e85ff11f51c725a1
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-05-16 18:01:07 -0500

    Query repository method to get a user by alias if it exists

--- social-multiplication/src/main/java/microservices/book/multiplication/repository/UserRepository.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/repository/UserRepository.java
@@ -3,5 +3,9 @@ package microservices.book.multiplication.repository;
 import microservices.book.multiplication.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByAlias(final String alias);
 }
