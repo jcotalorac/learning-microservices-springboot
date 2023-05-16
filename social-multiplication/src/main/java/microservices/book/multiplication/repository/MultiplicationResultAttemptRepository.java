cc3ca3a5fb74266dd797779de11be2c8fc6a0428
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-05-16 17:57:33 -0500

    Query repository method to get 5 top multiplication result attempts given a user alias

--- social-multiplication/src/main/java/microservices/book/multiplication/repository/MultiplicationResultAttemptRepository.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/repository/MultiplicationResultAttemptRepository.java
@@ -3,5 +3,9 @@ package microservices.book.multiplication.repository;
 import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MultiplicationResultAttemptRepository extends CrudRepository<MultiplicationResultAttempt, Long> {
+
+    List<MultiplicationResultAttempt> findTop5ByUserAliasOrderByIdDesc(String userAlias);
 }
