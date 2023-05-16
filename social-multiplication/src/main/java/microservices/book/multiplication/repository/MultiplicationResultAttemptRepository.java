10b8f89e33a19204861830e2b79e20185abe44b5
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-05-16 16:38:16 -0500

    Crud repository interface for multiplication result attempt entity

---
+++ social-multiplication/src/main/java/microservices/book/multiplication/repository/MultiplicationResultAttemptRepository.java
@@ -0,0 +1,7 @@
+package microservices.book.multiplication.repository;
+
+import microservices.book.multiplication.domain.MultiplicationResultAttempt;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MultiplicationResultAttemptRepository extends CrudRepository<MultiplicationResultAttempt, Long> {
+}
