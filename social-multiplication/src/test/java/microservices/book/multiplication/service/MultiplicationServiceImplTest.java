b2168253b71add4e1aab67d8d93031e5701191f9
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-05-18 21:50:15 -0500

    Mocking output for a non-existent database new user

--- social-multiplication/src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
+++ social-multiplication/src/test/java/microservices/book/multiplication/service/MultiplicationServiceImplTest.java
@@ -4,11 +4,14 @@ import microservices.book.multiplication.domain.Multiplication;
 import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 import microservices.book.multiplication.domain.User;
 import microservices.book.multiplication.repository.MultiplicationResultAttemptRepository;
+import microservices.book.multiplication.repository.UserRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -19,7 +22,8 @@ public class MultiplicationServiceImplTest {
 
     @Mock
     private RandomGeneratorService randomGeneratorService;
-
+    @Mock
+    private UserRepository userRepository;
     @Mock
     private MultiplicationResultAttemptRepository multiplicationResultAttemptRepository;
     private MultiplicationServiceImpl multiplicationService;
@@ -50,6 +54,8 @@ public class MultiplicationServiceImplTest {
         User user = new User("john_doe");
         MultiplicationResultAttempt attempt = new MultiplicationResultAttempt(user, multiplication, 3000, false);
         MultiplicationResultAttempt verifiedAttempt = new MultiplicationResultAttempt(user, multiplication, 3000, true);
+
+        given(userRepository.findByAlias("john_doe")).willReturn(Optional.empty());
 
         boolean attemptResult = multiplicationService.checkAttempt(attempt);
 
