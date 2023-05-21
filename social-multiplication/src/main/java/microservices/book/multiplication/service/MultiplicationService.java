8eed30207d21c2ade71f6992128a6b0d5c61527e
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-05-20 21:15:10 -0500

    Default implementation of service method to get user last multiplication result attempts

--- social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationService.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationService.java
@@ -3,9 +3,13 @@ package microservices.book.multiplication.service;
 import microservices.book.multiplication.domain.Multiplication;
 import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 
+import java.util.List;
+
 public interface MultiplicationService {
 
     Multiplication createRandomMultiplication();
 
     boolean checkAttempt(final MultiplicationResultAttempt multiplicationResultAttempt);
+
+    List<MultiplicationResultAttempt> getStatsForUser(final String userAlias);
 }
--- social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationServiceImpl.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -55,5 +56,10 @@ public class MultiplicationServiceImpl implements MultiplicationService {
 
         multiplicationResultAttemptRepository.save(checkedAttempt);
         return correct;
+    }
+
+    @Override
+    public List<MultiplicationResultAttempt> getStatsForUser(String userAlias) {
+        return null;
     }
 }
