4593af5f021349ad202a439581c2705513b9b3f5
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-06 10:10:24 -0500

    Provider method to get a queue exchange given a name

--- social-multiplication/src/main/java/microservices/book/multiplication/configuration/RabbitMQConfiguration.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/configuration/RabbitMQConfiguration.java
@@ -1,7 +1,12 @@
 package microservices.book.multiplication.configuration;
 
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfiguration {
+
+    public TopicExchange multiplicationExchange(final String exchangeName) {
+        return new TopicExchange(exchangeName);
+    }
 }
