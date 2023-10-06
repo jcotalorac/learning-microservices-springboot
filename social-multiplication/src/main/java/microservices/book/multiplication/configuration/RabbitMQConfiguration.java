c8ffaf7844dc40b21d113ec9727a99557954c70c
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-06 10:57:55 -0500

    Refactor to parameterize rabbit template depending on spring provided connection factory

--- social-multiplication/src/main/java/microservices/book/multiplication/configuration/RabbitMQConfiguration.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/configuration/RabbitMQConfiguration.java
@@ -1,6 +1,7 @@
 package microservices.book.multiplication.configuration;
 
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +15,8 @@ public class RabbitMQConfiguration {
         return new TopicExchange(exchangeName);
     }
 
-    public RabbitTemplate rabbitTemplate() {
-        return new RabbitTemplate();
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        return rabbitTemplate;
     }
 }
