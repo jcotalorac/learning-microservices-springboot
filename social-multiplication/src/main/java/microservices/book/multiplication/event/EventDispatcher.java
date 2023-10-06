6731a87a54ce484fcd4de3fc622983f36524be40
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-06 17:52:14 -0500

    Enabling event dipatcher as a Spring component to use in application

--- social-multiplication/src/main/java/microservices/book/multiplication/event/EventDispatcher.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/event/EventDispatcher.java
@@ -3,7 +3,9 @@ package microservices.book.multiplication.event;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class EventDispatcher {
 
     private RabbitTemplate rabbitTemplate;
