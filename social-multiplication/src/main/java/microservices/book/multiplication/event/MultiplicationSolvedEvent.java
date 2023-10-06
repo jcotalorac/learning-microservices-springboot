3048683078d66fdc17ada40ea5005a1316aeeac5
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-06 11:40:24 -0500

    Properties of a multiplication solved event

--- social-multiplication/src/main/java/microservices/book/multiplication/event/MultiplicationSolvedEvent.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/event/MultiplicationSolvedEvent.java
@@ -1,4 +1,8 @@
 package microservices.book.multiplication.event;
 
 public class MultiplicationSolvedEvent {
+
+    private final Long multiplicationResultAttemptId;
+    private final Long userId;
+    private final boolean correct;
 }
