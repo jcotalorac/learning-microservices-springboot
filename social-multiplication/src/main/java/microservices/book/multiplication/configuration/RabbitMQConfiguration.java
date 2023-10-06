bae244c835418d14526ae88fd5591173dea612ac
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-06 10:55:22 -0500

    Basic method to provide a rabbit template different than the default spring one

--- social-multiplication/src/main/java/microservices/book/multiplication/configuration/RabbitMQConfiguration.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/configuration/RabbitMQConfiguration.java
@@ -1,6 +1,7 @@
 package microservices.book.multiplication.configuration;
 
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +12,9 @@ public class RabbitMQConfiguration {
     @Bean
     public TopicExchange multiplicationExchange(@Value("${multiplication.exchange}") final String exchangeName) {
         return new TopicExchange(exchangeName);
+    }
+
+    public RabbitTemplate rabbitTemplate() {
+        return new RabbitTemplate();
     }
 }
