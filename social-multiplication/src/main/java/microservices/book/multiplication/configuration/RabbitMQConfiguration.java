b1ef749bb14c83e5792a05d0d17872f3f94305cd
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-06 11:02:51 -0500

    Configuration method to provide a json converter for queue message serialization

--- social-multiplication/src/main/java/microservices/book/multiplication/configuration/RabbitMQConfiguration.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/configuration/RabbitMQConfiguration.java
@@ -3,6 +3,7 @@ package microservices.book.multiplication.configuration;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +19,9 @@ public class RabbitMQConfiguration {
     public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
         final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         return rabbitTemplate;
+    }
+
+    public Jackson2JsonMessageConverter producerJackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
