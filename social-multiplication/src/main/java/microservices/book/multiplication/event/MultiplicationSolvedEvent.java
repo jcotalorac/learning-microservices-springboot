fe2fdd5017570f910aa15d46634047e2eb3730b9
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-06 11:46:20 -0500

    Lombok annotations for completing code of a POJO class

--- social-multiplication/src/main/java/microservices/book/multiplication/event/MultiplicationSolvedEvent.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/event/MultiplicationSolvedEvent.java
@@ -1,7 +1,16 @@
 package microservices.book.multiplication.event;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
 import java.io.Serializable;
 
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
 public class MultiplicationSolvedEvent implements Serializable {
 
     private final Long multiplicationResultAttemptId;
