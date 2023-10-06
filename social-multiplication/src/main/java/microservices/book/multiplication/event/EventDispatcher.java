925c822d52c30d5c2057ea7198cd524e3b03dd67
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-06 17:12:09 -0500

    Generating constructor with all properties

--- social-multiplication/src/main/java/microservices/book/multiplication/event/EventDispatcher.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/event/EventDispatcher.java
@@ -1,10 +1,18 @@
 package microservices.book.multiplication.event;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class EventDispatcher {
 
     private RabbitTemplate rabbitTemplate;
     private String multiplicationExchange;
     private String multiplicationSolvedRoutingKey;
+
+    @Autowired
+    public EventDispatcher(RabbitTemplate rabbitTemplate, String multiplicationExchange, String multiplicationSolvedRoutingKey) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.multiplicationExchange = multiplicationExchange;
+        this.multiplicationSolvedRoutingKey = multiplicationSolvedRoutingKey;
+    }
 }
