694ff296f92103887552d2169cf349aa57569413
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-06 17:32:36 -0500

    Enhancing clearing code for variable non-reassignment

--- social-multiplication/src/main/java/microservices/book/multiplication/event/EventDispatcher.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/event/EventDispatcher.java
@@ -2,6 +2,7 @@ package microservices.book.multiplication.event;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 public class EventDispatcher {
 
@@ -10,7 +11,9 @@ public class EventDispatcher {
     private String multiplicationSolvedRoutingKey;
 
     @Autowired
-    public EventDispatcher(RabbitTemplate rabbitTemplate, String multiplicationExchange, String multiplicationSolvedRoutingKey) {
+    public EventDispatcher(final RabbitTemplate rabbitTemplate,
+                           final String multiplicationExchange,
+                           final String multiplicationSolvedRoutingKey) {
         this.rabbitTemplate = rabbitTemplate;
         this.multiplicationExchange = multiplicationExchange;
         this.multiplicationSolvedRoutingKey = multiplicationSolvedRoutingKey;
