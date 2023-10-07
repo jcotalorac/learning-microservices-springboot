2268d054d439b3b5b047378476d7dd6d68684278
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-07 15:12:05 -0500

    Properties to model user badge won in certain time

--- gamification/src/main/java/microservices/book/gamification/domain/BadgeCard.java
+++ gamification/src/main/java/microservices/book/gamification/domain/BadgeCard.java
@@ -1,4 +1,9 @@
 package microservices.book.gamification.domain;
 
 public final class BadgeCard {
+
+    private final Long badgeId;
+    private final Long userId;
+    private final long badgeTimestamp;
+    private final Badge badge;
 }
