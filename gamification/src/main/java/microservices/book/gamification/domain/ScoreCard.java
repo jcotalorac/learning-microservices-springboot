a4b693f4a088f6e37abaaa3c595c0502af56ad16
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-07 18:42:32 -0500

    Properties for handling values of score attempt by an user at certain time

--- gamification/src/main/java/microservices/book/gamification/domain/ScoreCard.java
+++ gamification/src/main/java/microservices/book/gamification/domain/ScoreCard.java
@@ -3,4 +3,10 @@ package microservices.book.gamification.domain;
 public final class ScoreCard {
 
     private static final int DEFAULT_SCORE = 10;
+
+    private final Long cardId;
+    private final Long userId;
+    private final Long attemptId;
+    private final long scoreTimestamp;
+    private final int score;
 }
