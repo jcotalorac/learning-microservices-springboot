f0e9d3a9ada768c11aaaf8c51c79dab398340bf7
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-07 15:19:04 -0500

    Annotations for POJO class

--- gamification/src/main/java/microservices/book/gamification/domain/BadgeCard.java
+++ gamification/src/main/java/microservices/book/gamification/domain/BadgeCard.java
@@ -1,5 +1,14 @@
 package microservices.book.gamification.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
 public final class BadgeCard {
 
     private final Long badgeId;
