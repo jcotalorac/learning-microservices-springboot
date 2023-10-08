4bd0538a104fc9ccef0701512ea663dc56b1755b
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-08 13:31:05 -0500

    Declaring method to process a new attempt of the user and hold game stats

--- gamification/src/main/java/microservices/book/gamification/service/GameService.java
+++ gamification/src/main/java/microservices/book/gamification/service/GameService.java
@@ -1,4 +1,8 @@
 package microservices.book.gamification.service;
 
+import microservices.book.gamification.domain.GameStats;
+
 public interface GameService {
+
+    GameStats newAttemptForUser(Long userId, Long attemptId, boolean correct);
 }
