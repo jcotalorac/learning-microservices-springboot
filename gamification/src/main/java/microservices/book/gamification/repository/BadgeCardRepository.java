908a5055d4e72be9a89b48ba7c290e7503a103a9
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-08 12:07:58 -0500

    Method to retrieve all badges of a user, ordering them by most recent time

--- gamification/src/main/java/microservices/book/gamification/repository/BadgeCardRepository.java
+++ gamification/src/main/java/microservices/book/gamification/repository/BadgeCardRepository.java
@@ -1,7 +1,12 @@
 package microservices.book.gamification.repository;
 
+import microservices.book.gamification.domain.Badge;
 import microservices.book.gamification.domain.BadgeCard;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BadgeCardRepository extends CrudRepository<BadgeCard, Long> {
+
+    List<Badge> findByUserIdOrderByBadgeTimestampDesc(final Long userId);
 }
