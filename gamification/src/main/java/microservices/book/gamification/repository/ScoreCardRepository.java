c0f197376f46cd3d1a3c5f1e18cd0ce0f17084af
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-08 12:22:36 -0500

    Declaring method to retrieve total score of a user

--- gamification/src/main/java/microservices/book/gamification/repository/ScoreCardRepository.java
+++ gamification/src/main/java/microservices/book/gamification/repository/ScoreCardRepository.java
@@ -4,4 +4,6 @@ import microservices.book.gamification.domain.ScoreCard;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ScoreCardRepository extends CrudRepository<ScoreCard, Long> {
+
+    int getTotalScoreForUser(final Long userId);
 }
