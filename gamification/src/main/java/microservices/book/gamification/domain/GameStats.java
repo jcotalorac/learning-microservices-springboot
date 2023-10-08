5f7b3c08e64fe0fc9786e169c2b63c9e2aa150f6
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-07 20:30:08 -0500

    Annotations for POJO class

--- gamification/src/main/java/microservices/book/gamification/domain/GameStats.java
+++ gamification/src/main/java/microservices/book/gamification/domain/GameStats.java
@@ -1,7 +1,16 @@
 package microservices.book.gamification.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
 import java.util.List;
 
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
 public final class GameStats {
 
     private final Long userId;
