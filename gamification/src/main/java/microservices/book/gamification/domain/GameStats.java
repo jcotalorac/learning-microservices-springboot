c575cbadf1059b2e30547f001079e0f48e7d0167
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-07 20:25:43 -0500

    Properties to handle within game stats

--- gamification/src/main/java/microservices/book/gamification/domain/GameStats.java
+++ gamification/src/main/java/microservices/book/gamification/domain/GameStats.java
@@ -1,4 +1,10 @@
 package microservices.book.gamification.domain;
 
+import java.util.List;
+
 public final class GameStats {
+
+    private final Long userId;
+    private final int score;
+    private final List<Badge> badges;
 }
