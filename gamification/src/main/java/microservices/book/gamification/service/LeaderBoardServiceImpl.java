eae752b83827826a64e91b372ceb2db49948a70f
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-08 15:41:37 -0500

    Empty implementation of leaderboard service

---
+++ gamification/src/main/java/microservices/book/gamification/service/LeaderBoardServiceImpl.java
@@ -0,0 +1,12 @@
+package microservices.book.gamification.service;
+
+import microservices.book.gamification.domain.LeaderBoardRow;
+
+import java.util.List;
+
+public class LeaderBoardServiceImpl implements LeaderBoardService {
+    @Override
+    public List<LeaderBoardRow> getCurrentLeaderBoard() {
+        return null;
+    }
+}
