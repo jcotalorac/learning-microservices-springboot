d93c6b134e427d7922981ede52bbbe09b7d81934
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-08 13:35:28 -0500

    Declaring method to get the game statistics for a given user

--- gamification/src/main/java/microservices/book/gamification/service/GameService.java
+++ gamification/src/main/java/microservices/book/gamification/service/GameService.java
@@ -5,4 +5,6 @@ import microservices.book.gamification.domain.GameStats;
 public interface GameService {
 
     GameStats newAttemptForUser(Long userId, Long attemptId, boolean correct);
+
+    GameStats retrieveStatsForUser(Long userId);
 }
