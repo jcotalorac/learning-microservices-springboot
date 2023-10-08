b26e02755b923eef28b9df1f1ca22edcc48f0376
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-08 15:49:16 -0500

    Generating test class for game service implementation

---
+++ gamification/src/test/java/microservices/book/gamification/service/GameServiceImplTest.java
@@ -0,0 +1,16 @@
+package microservices.book.gamification.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServiceImplTest {
+
+    @Test
+    void newAttemptForUser() {
+    }
+
+    @Test
+    void retrieveStatsForUser() {
+    }
+}
