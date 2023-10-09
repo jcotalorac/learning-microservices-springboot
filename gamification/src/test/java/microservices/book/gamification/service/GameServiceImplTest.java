87971c8370966a2ad60f8be08d83724cc522da4a
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-09 17:00:30 -0500

    Initializing service under tests for all test cases of the suite

--- gamification/src/test/java/microservices/book/gamification/service/GameServiceImplTest.java
+++ gamification/src/test/java/microservices/book/gamification/service/GameServiceImplTest.java
@@ -1,11 +1,13 @@
 package microservices.book.gamification.service;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class GameServiceImplTest {
 
     private GameServiceImpl gameService;
 
+    @BeforeAll
     public void setUp() {
         gameService = new GameServiceImpl();
     }
