873e2c5c8bbec8ba4f4ae63515c7cf374c374ba6
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-12 18:09:03 -0500

    Game service returning of game stats previous query of score card of the user to get total score

--- gamification/src/main/java/microservices/book/gamification/service/GameServiceImpl.java
+++ gamification/src/main/java/microservices/book/gamification/service/GameServiceImpl.java
@@ -1,11 +1,18 @@
 package microservices.book.gamification.service;
 
 import microservices.book.gamification.domain.GameStats;
+import microservices.book.gamification.repository.ScoreCardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class GameServiceImpl implements GameService {
+
+    @Autowired
+    private ScoreCardRepository scoreCardRepository;
+
     @Override
     public GameStats newAttemptForUser(Long userId, Long attemptId, boolean correct) {
-        return new GameStats();
+        int totalScoreForUser = scoreCardRepository.getTotalScoreForUser(userId);
+        return new GameStats(userId, totalScoreForUser, null);
     }
 
     @Override
