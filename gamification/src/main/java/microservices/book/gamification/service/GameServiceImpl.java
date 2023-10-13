795a280d1e838623e3ef49d8c1fa0a91aa8b8637
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-12 21:11:40 -0500

    Getting user badges from repository

--- gamification/src/main/java/microservices/book/gamification/service/GameServiceImpl.java
+++ gamification/src/main/java/microservices/book/gamification/service/GameServiceImpl.java
@@ -2,13 +2,16 @@ package microservices.book.gamification.service;
 
 import microservices.book.gamification.domain.Badge;
 import microservices.book.gamification.domain.GameStats;
+import microservices.book.gamification.repository.BadgeCardRepository;
 import microservices.book.gamification.repository.ScoreCardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class GameServiceImpl implements GameService {
     private ScoreCardRepository scoreCardRepository;
+    private BadgeCardRepository badgeCardRepository;
 
     @Autowired
     public GameServiceImpl(ScoreCardRepository scoreCardRepository) {
@@ -18,8 +21,7 @@ public class GameServiceImpl implements GameService {
     @Override
     public GameStats newAttemptForUser(Long userId, Long attemptId, boolean correct) {
         int totalScoreForUser = scoreCardRepository.getTotalScoreForUser(userId);
-        ArrayList<Badge> badges = new ArrayList<>();
-        badges.add(Badge.FIRST_WON);
+        List<Badge> badges = badgeCardRepository.findByUserIdOrderByBadgeTimestampDesc(userId);
         return new GameStats(userId, totalScoreForUser, badges);
     }
 
