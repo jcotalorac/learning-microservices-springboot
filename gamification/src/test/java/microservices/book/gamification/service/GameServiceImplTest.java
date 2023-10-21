38c0a99f630dbc7f5f96297fcbde0b4152b4385a
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-21 18:56:06 -0500

    Cleaning code

--- gamification/src/test/java/microservices/book/gamification/service/GameServiceImplTest.java
+++ gamification/src/test/java/microservices/book/gamification/service/GameServiceImplTest.java
@@ -14,7 +14,6 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
