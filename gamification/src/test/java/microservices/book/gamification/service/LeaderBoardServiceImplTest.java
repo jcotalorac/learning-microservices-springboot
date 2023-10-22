fbc41d4ced751d5e82dd4e9b30a4e42e78bca8b7
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-22 14:20:09 -0500

    Mocking dependency of score card repository

--- gamification/src/test/java/microservices/book/gamification/service/LeaderBoardServiceImplTest.java
+++ gamification/src/test/java/microservices/book/gamification/service/LeaderBoardServiceImplTest.java
@@ -1,16 +1,24 @@
 package microservices.book.gamification.service;
 
+import microservices.book.gamification.repository.ScoreCardRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mock;
+
+import static org.mockito.MockitoAnnotations.initMocks;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class LeaderBoardServiceImplTest {
+
+    @Mock
+    private ScoreCardRepository scoreCardRepository;
 
     private LeaderBoardServiceImpl leaderBoardService;
 
     @BeforeAll
     public void setUp() {
+        initMocks(this);
         leaderBoardService = new LeaderBoardServiceImpl();
     }
 
