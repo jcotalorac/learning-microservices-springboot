7f6c5dcafb7c462c361bd42d6422e7bcdf36ce7f
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-22 12:24:57 -0500

    Generating default class for test of leader board service

---
+++ gamification/src/test/java/microservices/book/gamification/service/LeaderBoardServiceImplTest.java
@@ -0,0 +1,17 @@
+package microservices.book.gamification.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeaderBoardServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getCurrentLeaderBoard() {
+    }
+}
