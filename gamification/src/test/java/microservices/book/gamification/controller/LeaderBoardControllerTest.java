f0d5f5e13891f27e5c2d051cfca4c537288cbbba
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-23 14:19:48 -0500

    Precondition simulating data returned from the leader board service

--- gamification/src/test/java/microservices/book/gamification/controller/LeaderBoardControllerTest.java
+++ gamification/src/test/java/microservices/book/gamification/controller/LeaderBoardControllerTest.java
@@ -1,11 +1,16 @@
 package microservices.book.gamification.controller;
 
+import microservices.book.gamification.domain.LeaderBoardRow;
 import microservices.book.gamification.service.LeaderBoardServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mock;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.mockito.BDDMockito.given;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -23,5 +28,11 @@ public class LeaderBoardControllerTest {
     }
 
     @Test
-    public void retrieveLeaderBoardTest() {}
+    public void retrieveLeaderBoardTest() {
+
+        given(leaderBoardService.getCurrentLeaderBoard())
+                .willReturn(IntStream.range(1, 11)
+                        .mapToObj(i -> new LeaderBoardRow((long) i, (long) (10 - i)))
+                        .collect(Collectors.toList()));
+    }
 }
