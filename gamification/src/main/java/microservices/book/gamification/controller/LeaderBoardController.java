6ae269ff246beffa879922df6cec2f824f7d1157
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-23 10:23:26 -0500

    Declaring rest controller for leader board with url resource

--- gamification/src/main/java/microservices/book/gamification/controller/LeaderBoardController.java
+++ gamification/src/main/java/microservices/book/gamification/controller/LeaderBoardController.java
@@ -1,4 +1,9 @@
 package microservices.book.gamification.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/leaders")
 public class LeaderBoardController {
 }
