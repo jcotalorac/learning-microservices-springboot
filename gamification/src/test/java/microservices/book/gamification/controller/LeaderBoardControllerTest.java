d50a4921bf63fd5dbda162e37109705fdc6b5495
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-23 10:54:29 -0500

    Test class generated for leader board controller

---
+++ gamification/src/test/java/microservices/book/gamification/controller/LeaderBoardControllerTest.java
@@ -0,0 +1,12 @@
+package microservices.book.gamification.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeaderBoardControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
