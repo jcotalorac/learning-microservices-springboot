23d53228a8e2fdded18844bc2d920864293332ce
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-23 11:30:57 -0500

    Including leader board service dependency

--- gamification/src/main/java/microservices/book/gamification/controller/LeaderBoardController.java
+++ gamification/src/main/java/microservices/book/gamification/controller/LeaderBoardController.java
@@ -1,6 +1,7 @@
 package microservices.book.gamification.controller;
 
 import microservices.book.gamification.domain.LeaderBoardRow;
+import microservices.book.gamification.service.LeaderBoardService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/leaders")
 public class LeaderBoardController {
+
+    private LeaderBoardService leaderBoardService;
+    public LeaderBoardController(LeaderBoardService leaderBoardService) {
+        this.leaderBoardService = leaderBoardService;
+    }
 
     @GetMapping
     public List<LeaderBoardRow> getLeaderBoard(){
--- gamification/src/test/java/microservices/book/gamification/controller/LeaderBoardControllerTest.java
+++ gamification/src/test/java/microservices/book/gamification/controller/LeaderBoardControllerTest.java
@@ -1,19 +1,25 @@
 package microservices.book.gamification.controller;
 
+import microservices.book.gamification.service.LeaderBoardServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mock;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class LeaderBoardControllerTest {
+
+    @Mock
+    private LeaderBoardServiceImpl leaderBoardService;
 
     private LeaderBoardController leaderBoardController;
 
     @BeforeEach
     public void setUp() {
-        leaderBoardController = new LeaderBoardController();
+        initMocks(this);
+        leaderBoardController = new LeaderBoardController(leaderBoardService);
     }
 
     @Test
