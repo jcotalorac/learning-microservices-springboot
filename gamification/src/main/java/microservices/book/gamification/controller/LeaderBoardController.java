66084c2817dd91860f81ac988466f9e3cfe3aa82
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-23 11:17:29 -0500

    Default implementation of method to expose leader board

--- gamification/src/main/java/microservices/book/gamification/controller/LeaderBoardController.java
+++ gamification/src/main/java/microservices/book/gamification/controller/LeaderBoardController.java
@@ -1,9 +1,18 @@
 package microservices.book.gamification.controller;
 
+import microservices.book.gamification.domain.LeaderBoardRow;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/leaders")
 public class LeaderBoardController {
+
+    @GetMapping
+    public List<LeaderBoardRow> getLeaderBoard(){
+        return null;
+    }
 }
