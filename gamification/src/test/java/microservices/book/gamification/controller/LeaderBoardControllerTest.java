8faf222572a5e05db2c4fb862aec1f39aa00d418
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-23 11:20:55 -0500

    Test case for leader board retrieving

--- gamification/src/test/java/microservices/book/gamification/controller/LeaderBoardControllerTest.java
+++ gamification/src/test/java/microservices/book/gamification/controller/LeaderBoardControllerTest.java
@@ -1,6 +1,7 @@
 package microservices.book.gamification.controller;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,4 +15,7 @@ public class LeaderBoardControllerTest {
     public void setUp() {
         leaderBoardController = new LeaderBoardController();
     }
+
+    @Test
+    public void retrieveLeaderBoardTest() {}
 }
