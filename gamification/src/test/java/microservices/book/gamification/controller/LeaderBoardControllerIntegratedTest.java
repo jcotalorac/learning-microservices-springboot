6d7cbe424f23915f407eadd9ca47841fcebe1284
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-24 18:22:01 -0500

    Enabling class as integrated test and mock variable

--- gamification/src/test/java/microservices/book/gamification/controller/LeaderBoardControllerIntegratedTest.java
+++ gamification/src/test/java/microservices/book/gamification/controller/LeaderBoardControllerIntegratedTest.java
@@ -2,10 +2,17 @@ package microservices.book.gamification.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(LeaderBoardControllerIntegratedTest.class)
 public class LeaderBoardControllerIntegratedTest {
+
+    @Autowired
+    private MockMvc mvc;
 
     @BeforeEach
     public void setUp() {
