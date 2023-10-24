46490e9020a8135f947d8621d390e6ad64468837
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-24 18:51:10 -0500

    Initializing json serializer for endpoint output

--- gamification/src/test/java/microservices/book/gamification/controller/LeaderBoardControllerIntegratedTest.java
+++ gamification/src/test/java/microservices/book/gamification/controller/LeaderBoardControllerIntegratedTest.java
@@ -1,10 +1,15 @@
 package microservices.book.gamification.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import microservices.book.gamification.domain.LeaderBoardRow;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,8 +19,11 @@ public class LeaderBoardControllerIntegratedTest {
     @Autowired
     private MockMvc mvc;
 
+    private JacksonTester<List<LeaderBoardRow>> json;
+
     @BeforeEach
     public void setUp() {
+        JacksonTester.initFields(this, ObjectMapper::new);
     }
 
     @Test
