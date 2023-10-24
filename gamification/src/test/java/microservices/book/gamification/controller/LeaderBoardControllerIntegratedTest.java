f753e348d21b284c4032f9be45de1ed57f576d5b
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-24 17:41:51 -0500

    Generating integration test for leader board controller

---
+++ gamification/src/test/java/microservices/book/gamification/controller/LeaderBoardControllerIntegratedTest.java
@@ -0,0 +1,17 @@
+package microservices.book.gamification.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeaderBoardControllerIntegratedTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getLeaderBoard() {
+    }
+}
