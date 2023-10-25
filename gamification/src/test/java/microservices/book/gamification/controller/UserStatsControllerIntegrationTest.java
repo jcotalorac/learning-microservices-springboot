84491b26071c782c91e4c9703620d4451dc5da39
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-24 22:00:27 -0500

    JSON Serializer for output of user stats controller

--- gamification/src/test/java/microservices/book/gamification/controller/UserStatsControllerIntegrationTest.java
+++ gamification/src/test/java/microservices/book/gamification/controller/UserStatsControllerIntegrationTest.java
@@ -1,9 +1,11 @@
 package microservices.book.gamification.controller;
 
+import microservices.book.gamification.domain.GameStats;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,6 +15,8 @@ public class UserStatsControllerIntegrationTest {
 
     @Autowired
     private MockMvc mvc;
+
+    private JacksonTester<GameStats> json;
 
     @BeforeEach
     public void setUp() {
