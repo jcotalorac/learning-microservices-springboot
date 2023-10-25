e0b5777fb4df37a621f2a457dc497afcd03e1cc2
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-25 17:29:15 -0500

    Implementing messages factory

--- gamification/src/main/java/microservices/book/gamification/configuration/RabbitMQConfiguration.java
+++ gamification/src/main/java/microservices/book/gamification/configuration/RabbitMQConfiguration.java
@@ -3,9 +3,15 @@ package microservices.book.gamification.configuration;
 import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 
 @Configuration
 public class RabbitMQConfiguration implements RabbitListenerConfigurer {
+
+    public DefaultMessageHandlerMethodFactory messageHandlerMethodFactory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        return factory;
+    }
     @Override
     public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
 
