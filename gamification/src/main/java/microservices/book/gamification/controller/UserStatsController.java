226d196f633bd8b8e571bad21edbccf143fdafb1
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-24 22:43:49 -0500

    Enabling REST controller to expose stats under get http verb

--- gamification/src/main/java/microservices/book/gamification/controller/UserStatsController.java
+++ gamification/src/main/java/microservices/book/gamification/controller/UserStatsController.java
@@ -2,7 +2,12 @@ package microservices.book.gamification.controller;
 
 import microservices.book.gamification.domain.GameStats;
 import microservices.book.gamification.service.GameService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/stats")
 public class UserStatsController {
 
     private GameService gameService;
@@ -11,6 +16,7 @@ public class UserStatsController {
         this.gameService = gameService;
     }
 
+    @GetMapping
     public GameStats getStatsForUser(Long userId) {
         return gameService.retrieveStatsForUser(userId);
     }
