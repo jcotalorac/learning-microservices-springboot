45cb2818fef9a157c1cd35bc907dcaf2fac52f41
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-25 16:21:39 -0500

    Default Rabbit listeners configuration to implement

--- gamification/src/main/java/microservices/book/gamification/configuration/RabbitMQConfiguration.java
+++ gamification/src/main/java/microservices/book/gamification/configuration/RabbitMQConfiguration.java
@@ -1,7 +1,13 @@
 package microservices.book.gamification.configuration;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class RabbitMQConfiguration {
+public class RabbitMQConfiguration implements RabbitListenerConfigurer {
+    @Override
+    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
+
+    }
 }
