0d4a6f9b958e670c377e36349d66c0ffa75f4309
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-24 20:22:09 -0500

    Verifying http ok status of the response

--- gamification/src/test/java/microservices/book/gamification/controller/LeaderBoardControllerIntegratedTest.java
+++ gamification/src/test/java/microservices/book/gamification/controller/LeaderBoardControllerIntegratedTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +18,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
@@ -45,5 +47,7 @@ public class LeaderBoardControllerIntegratedTest {
 
         MockHttpServletResponse response = mvc.perform(get("/leaders")
                 .accept(MediaType.APPLICATION_JSON)).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
     }
 }
