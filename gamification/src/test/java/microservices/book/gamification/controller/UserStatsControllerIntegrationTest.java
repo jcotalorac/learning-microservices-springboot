3a4bab8cf7bf0a2c37e3fe1d626a80bee3dfe5ea
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-24 22:06:44 -0500

    Injecting mock of game service into user stats controller context

--- gamification/src/test/java/microservices/book/gamification/controller/UserStatsControllerIntegrationTest.java
+++ gamification/src/test/java/microservices/book/gamification/controller/UserStatsControllerIntegrationTest.java
@@ -2,15 +2,20 @@ package microservices.book.gamification.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import microservices.book.gamification.domain.GameStats;
+import microservices.book.gamification.service.GameService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(UserStatsController.class)
 public class UserStatsControllerIntegrationTest {
+
+    @MockBean
+    private GameService gameService;
 
     @Autowired
     private MockMvc mvc;
