ea88535155bc5c6ede8afae172633758b874ac71
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-25 18:03:30 -0500

    Setting queue name from externalized value

--- gamification/src/main/java/microservices/book/gamification/configuration/RabbitMQConfiguration.java
+++ gamification/src/main/java/microservices/book/gamification/configuration/RabbitMQConfiguration.java
@@ -19,7 +19,7 @@ public class RabbitMQConfiguration implements RabbitListenerConfigurer {
     }
 
     @Bean
-    public Queue gamificationMultiplicationQueue(final String queueName) {
+    public Queue gamificationMultiplicationQueue(@Value("${multiplication.queue}") final String queueName) {
         return new Queue(queueName, true);
     }
 
