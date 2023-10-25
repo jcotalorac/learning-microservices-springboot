9a9d8367c6d79b431caf048ee035cfa7e5bb9695
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-24 21:57:26 -0500

    Enabling class as integration test component to rise context for user stats controller

--- gamification/src/test/java/microservices/book/gamification/controller/UserStatsControllerIntegrationTest.java
+++ gamification/src/test/java/microservices/book/gamification/controller/UserStatsControllerIntegrationTest.java
@@ -2,10 +2,17 @@ package microservices.book.gamification.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(UserStatsController.class)
 public class UserStatsControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
 
     @BeforeEach
     public void setUp() {
