d02c931222ddab42982c8a523cfff37eb60a3a44
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-25 17:36:46 -0500

    Implementing serializer for messages

--- gamification/src/main/java/microservices/book/gamification/configuration/RabbitMQConfiguration.java
+++ gamification/src/main/java/microservices/book/gamification/configuration/RabbitMQConfiguration.java
@@ -4,11 +4,15 @@ import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 
 @Configuration
 public class RabbitMQConfiguration implements RabbitListenerConfigurer {
 
+    public MappingJackson2MessageConverter consumer() {
+        return new MappingJackson2MessageConverter();
+    }
     @Bean
     public DefaultMessageHandlerMethodFactory messageHandlerMethodFactory() {
         DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
