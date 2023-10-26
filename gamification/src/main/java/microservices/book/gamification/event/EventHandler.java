038caf460b250895099fbc81992658e2f2c766f6
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-25 21:01:58 -0500

    Service triggered to process event received

--- gamification/src/main/java/microservices/book/gamification/event/EventHandler.java
+++ gamification/src/main/java/microservices/book/gamification/event/EventHandler.java
@@ -1,9 +1,12 @@
 package microservices.book.gamification.event;
 
 import lombok.extern.slf4j.Slf4j;
+import microservices.book.gamification.service.GameService;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class EventHandler {
+
+    private GameService gameService;
 }
