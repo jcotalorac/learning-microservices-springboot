eddd954397f8934ff7eca4f6dae0db6f5035eaf8
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-26 21:33:38 -0500

    Test case for results given id of the attempt

--- social-multiplication/src/test/java/microservices/book/multiplication/controller/MultiplicationResultAttemptControllerTest.java
+++ social-multiplication/src/test/java/microservices/book/multiplication/controller/MultiplicationResultAttemptControllerTest.java
@@ -96,4 +96,7 @@ public class MultiplicationResultAttemptControllerTest {
                                 attempt.getResultAttempt(), correct))
                         .getJson());
     }
+
+    @Test
+    public void getResultsByIdTest() {}
 }
