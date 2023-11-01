c7e8e88a72da47c31f922390bb91a5e2fe8fe618
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-31 21:10:48 -0500

    Empty constructor for JSON/JPA

--- gamification/src/main/java/microservices/book/gamification/client/dto/MultiplicationResultAttempt.java
+++ gamification/src/main/java/microservices/book/gamification/client/dto/MultiplicationResultAttempt.java
@@ -14,8 +14,16 @@ public final class MultiplicationResultAttempt {
     private final String userAlias;
 
     private final int multiplicationFactorA;
-    private final int getMultiplicationFactorB;
+    private final int multiplicationFactorB;
     private final int resultAttempt;
 
     private final boolean correct;
+
+    public MultiplicationResultAttempt() {
+        userAlias = null;
+        multiplicationFactorA = -1;
+        multiplicationFactorB = -1;
+        resultAttempt = -1;
+        correct = false;
+    }
 }
