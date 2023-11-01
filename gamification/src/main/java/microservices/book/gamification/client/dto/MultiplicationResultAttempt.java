b3b34cb9818d7bb47096c47be08db98a8feb43c3
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-10-31 21:06:10 -0500

    Model copy from social multiplication microservice, but only preserving the needed information

--- gamification/src/main/java/microservices/book/gamification/client/dto/MultiplicationResultAttempt.java
+++ gamification/src/main/java/microservices/book/gamification/client/dto/MultiplicationResultAttempt.java
@@ -1,4 +1,12 @@
 package microservices.book.gamification.client.dto;
 
 public final class MultiplicationResultAttempt {
+
+    private final String userAlias;
+
+    private final int multiplicationFactorA;
+    private final int getMultiplicationFactorB;
+    private final int resultAttempt;
+
+    private final boolean correct;
 }
