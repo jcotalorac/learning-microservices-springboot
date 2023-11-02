5721a14d576180f4795c446a17a7f8dc00800e58
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-11-01 21:46:04 -0500

    Default method to deserialize a comming attempt from the social multiplication microservice into the client gamification service representation

--- gamification/src/main/java/microservices/book/gamification/client/MultiplicationResultAttemptDeserializer.java
+++ gamification/src/main/java/microservices/book/gamification/client/MultiplicationResultAttemptDeserializer.java
@@ -1,4 +1,16 @@
 package microservices.book.gamification.client;
 
-public class MultiplicationResultAttemptDeserializer {
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import microservices.book.gamification.client.dto.MultiplicationResultAttempt;
+
+import java.io.IOException;
+
+public class MultiplicationResultAttemptDeserializer extends JsonDeserializer<MultiplicationResultAttempt> {
+    @Override
+    public MultiplicationResultAttempt deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+        return null;
+    }
 }
