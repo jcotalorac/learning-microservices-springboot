256e3f7448e807b99ebfa45c9d8be3197df89de3
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-11-03 10:01:41 -0500

    Getting de JSON node ready to extract information

--- gamification/src/main/java/microservices/book/gamification/client/MultiplicationResultAttemptDeserializer.java
+++ gamification/src/main/java/microservices/book/gamification/client/MultiplicationResultAttemptDeserializer.java
@@ -2,6 +2,8 @@ package microservices.book.gamification.client;
 
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import microservices.book.gamification.client.dto.MultiplicationResultAttempt;
@@ -9,8 +11,11 @@ import microservices.book.gamification.client.dto.MultiplicationResultAttempt;
 import java.io.IOException;
 
 public class MultiplicationResultAttemptDeserializer extends JsonDeserializer<MultiplicationResultAttempt> {
+
     @Override
     public MultiplicationResultAttempt deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+        ObjectCodec objectCodec = p.getCodec();
+        TreeNode treeNode = objectCodec.readTree(p);
         return null;
     }
 }
