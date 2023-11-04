ca463b380e801b721041dd523cbbe4ca4fab6853
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-11-03 21:40:35 -0500

    Method producer of rest template instance

--- gamification/src/main/java/microservices/book/gamification/configuration/RestClientConfiguration.java
+++ gamification/src/main/java/microservices/book/gamification/configuration/RestClientConfiguration.java
@@ -1,4 +1,11 @@
 package microservices.book.gamification.configuration;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
+
 public class RestClientConfiguration {
+
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 }
