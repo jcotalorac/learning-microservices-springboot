8a1e90954f12c2ca02b60929f981ff3c7536b2c7
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-11-03 21:51:56 -0500

    Marking class a spring component available to use in the application

--- gamification/src/main/java/microservices/book/gamification/client/MultiplicationResultAttemptClientImpl.java
+++ gamification/src/main/java/microservices/book/gamification/client/MultiplicationResultAttemptClientImpl.java
@@ -1,7 +1,9 @@
 package microservices.book.gamification.client;
 
 import microservices.book.gamification.client.dto.MultiplicationResultAttempt;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MultiplicationResultAttemptClientImpl implements MultiplicationResultAttemptClient {
 
     @Override
