1a34c9e1bc89283d917292774129aedff4ad0a40
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-11-08 17:05:13 -0500

    Printing user id and user score

--- ui/webapps/ui/gamification-client.js
+++ ui/webapps/ui/gamification-client.js
@@ -3,6 +3,11 @@ function updateLeaderBoard() {
         url: "http://localhost:8081/leaders"
     }).then(function(data){
         $('#leaderboard-body').empty();
-        data.forEach();
+        data.forEach(function(row){
+            $('#leaderboard-body').append('<tr>' +
+            '<td>' + row.userId + '</td>' +
+            '<td>' + row.totalScore + '</td>' +
+            '</tr>');
+        });
     });
 }
