ab502d8f2b0f46a21b197ad307cf272fe8d7b174
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-11-08 19:49:53 -0500

    Adding pattern to registry to allow incoming different hosts/ports requets enabling cross domain

--- social-multiplication/src/main/java/microservices/book/multiplication/configuration/WebConfiguration.java
+++ social-multiplication/src/main/java/microservices/book/multiplication/configuration/WebConfiguration.java
@@ -1,6 +1,12 @@
 package microservices.book.multiplication.configuration;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
