7de41641e9f18f87f2bc9913c9461c8cb2ae3ee1
Juan Carlos Otálora <jcotalorac@gmail.com>
2023-11-15 17:25:33 -0500

    Including mapping patterns to allow differents hosts/ports origins for cross domains requests

--- gateway/src/main/java/microservices/book/gateway/configuration/WebConfiguration.java
+++ gateway/src/main/java/microservices/book/gateway/configuration/WebConfiguration.java
@@ -1,6 +1,12 @@
 package microservices.book.gateway.configuration;
 
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 public class WebConfiguration implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
