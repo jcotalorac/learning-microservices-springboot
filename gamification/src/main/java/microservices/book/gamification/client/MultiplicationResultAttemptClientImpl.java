2a3727b3f0e2f5689dde3522b1121d0922c8ea3a
Juan Carlos Otálora <jcotalorac@gmail.com>
2024-01-01 17:45:50 -0500

    Annotating method to provide circuit breaker fallback process to return a default response if there is a communication fails with social-multiplication results endpoint

--- gamification/src/main/java/microservices/book/gamification/client/MultiplicationResultAttemptClientImpl.java
+++ gamification/src/main/java/microservices/book/gamification/client/MultiplicationResultAttemptClientImpl.java
@@ -1,5 +1,6 @@
 package microservices.book.gamification.client;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import microservices.book.gamification.client.dto.MultiplicationResultAttempt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,9 +20,14 @@ public class MultiplicationResultAttemptClientImpl implements MultiplicationResu
         this.multiplicationHost = multiplicationHost;
     }
 
+    @HystrixCommand(fallbackMethod = "defaultResult")
     @Override
     public MultiplicationResultAttempt retrieveMultiplicationResultAttemptById(Long multiplicationId) {
         return restTemplate.getForObject(multiplicationHost + "/results/" + multiplicationId,
                 MultiplicationResultAttempt.class);
+    }
+
+    private MultiplicationResultAttempt defaultResult(Long multiplicationId) {
+        return new MultiplicationResultAttempt("fakeAlias", 10, 10, 100, true);
     }
 }
