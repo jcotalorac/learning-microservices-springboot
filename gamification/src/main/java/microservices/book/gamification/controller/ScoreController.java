14bbfa900259443c36457d9040e0f0dfc6cbf32e
Juan Carlos Otálora <jcotalorac@gmail.com>
2024-02-05 18:16:51 -0500

    Implementing score controller invoking score for attempt service

--- gamification/src/main/java/microservices/book/gamification/controller/ScoreController.java
+++ gamification/src/main/java/microservices/book/gamification/controller/ScoreController.java
@@ -1,7 +1,9 @@
 package microservices.book.gamification.controller;
 
 import microservices.book.gamification.domain.ScoreCard;
+import microservices.book.gamification.service.GameService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/scores")
 public class ScoreController {
 
+    private final GameService gameService;
+
+    public ScoreController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
     @GetMapping("/{attemptId}")
-    public ScoreCard getScoreForAttempt() {
-        return new ScoreCard();
+    public ScoreCard getScoreForAttempt(@PathVariable("attemptId") final Long attemptId) {
+        return gameService.getScoreForAttempt(attemptId);
     }
 }
